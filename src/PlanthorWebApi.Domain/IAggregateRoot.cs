1617d6fadb895821d85dec16fcffbd2691f9664f
Pham Le Trung <plt@netcompany.com>
2024-03-02 13:03:17 +0700

    feat: setup DDD IAggregateRoot

---
+++ src/PlanthorWebApi.Domain/IAggregateRoot.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace PlanthorWebApi.Domain
+
+{
+    /// <summary>
+    /// Represents an aggregate root in the domain model.
+    /// </summary>
+    /// <remarks>
+    /// In Domain-Driven Design (DDD), an aggregate root is an entity that acts as a boundary for a group of related entities.
+    /// It is responsible for maintaining the consistency and integrity of the entities within the aggregate.
+    /// </remarks>
+    public interface IAggregateRoot { }
+}
