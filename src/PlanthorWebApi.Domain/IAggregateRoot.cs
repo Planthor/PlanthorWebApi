0d444704ac9e184a5b09dd79ef45ae3a63f71e65
Phạm Lê Trung <plt@netcompany.com>
2024-03-10 22:22:15 +0700

    feat: refactor

--- src/PlanthorWebApi.Domain/IAggregateRoot.cs
+++ src/PlanthorWebApi.Domain/IAggregateRoot.cs
@@ -1,4 +1,6 @@
-﻿namespace PlanthorWebApi.Domain;
+﻿using System.Collections.Generic;
+
+namespace PlanthorWebApi.Domain;
 
 /// <summary>
 /// Represents an aggregate root in the domain model.
@@ -7,4 +9,11 @@
 /// In Domain-Driven Design (DDD), an aggregate root is an entity that acts as a boundary for a group of related entities.
 /// It is responsible for maintaining the consistency and integrity of the entities within the aggregate.
 /// </remarks>
-public interface IAggregateRoot { }
+public interface IAggregateRoot
+{
+    /// <summary>
+    /// Validates the aggregate root and returns a collection of validation messages.
+    /// </summary>
+    /// <returns>A collection of validation messages.</returns>
+    IEnumerable<string> Validate();
+}
