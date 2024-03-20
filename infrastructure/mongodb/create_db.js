53414a80c39185160b90beed751ae0fa99e3f7be
Phạm Lê Trung <plt@netcompany.com>
2024-03-20 12:50:16 +0700

    chore: fix suggestion

--- infrastructure/mongodb/create_db.js
+++ infrastructure/mongodb/create_db.js
@@ -2,4 +2,4 @@
 
 // MongoDB is fundamentally designed for "create on first use"
 // If you do not insert data with your JavaScript files, then no database is created.
-db.createCollection("default");
+db.createCollection("planthor");
