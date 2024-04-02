0cca01b026c6c8bb7e76164b7b7d405efa2f7f2f
Phạm Lê Trung <plt@netcompany.com>
2024-04-02 20:57:14 +0700

    fix: add more test coverage

--- tests/UnitTests/PlanthorWebApi.Domain.Tests/Shared/ValueObjectTests.cs
+++ tests/UnitTests/PlanthorWebApi.Domain.Tests/Shared/ValueObjectTests.cs
@@ -21,8 +21,103 @@ public class ValueObjectTests
         };
 
         // Act & Assert
-        Assert.True(valueObject1.Equals(valueObject2));
         Assert.Equal(valueObject1.GetHashCode(), valueObject2.GetHashCode());
+    }
+
+    [Fact]
+    public void WithDifferentValuesHaveDifferentHashCode()
+    {
+        // Arrange
+        var valueObject1 = new TestValueObject
+        {
+            Value = 1
+        };
+
+        var valueObject2 = new TestValueObject
+        {
+            Value = 2
+        };
+
+        // Act & Assert
+        Assert.NotEqual(valueObject1.GetHashCode(), valueObject2.GetHashCode());
+    }
+
+    [Fact]
+    public void WithSameValuesEqualsReturnTrue()
+    {
+        // Arrange
+        var valueObject1 = new TestValueObject
+        {
+            Value = 1
+        };
+
+        var valueObject2 = new TestValueObject
+        {
+            Value = 1
+        };
+
+        // Act
+        var result = valueObject1.Equals(valueObject2);
+
+        // Act & Assert
+        Assert.True(result);
+    }
+
+    [Fact]
+    public void WithDifferentValuesEqualsReturnFalse()
+    {
+        // Arrange
+        var valueObject1 = new TestValueObject
+        {
+            Value = 1
+        };
+
+        var valueObject2 = new TestValueObject
+        {
+            Value = 2
+        };
+
+        // Act
+        var result = valueObject1.Equals(valueObject2);
+
+        // Act & Assert
+        Assert.False(result);
+    }
+
+    [Fact]
+    public void WithDifferentTypesEqualsReturnFalse()
+    {
+        // Arrange
+        var valueObject1 = new TestValueObject
+        {
+            Value = 1
+        };
+
+        int valueObject2 = 1;
+
+        // Act
+        var result = valueObject1.Equals(valueObject2);
+
+        // Act & Assert
+        Assert.False(result);
+    }
+
+    [Fact]
+    public void WithNullObjectEqualsReturnFalse()
+    {
+        // Arrange
+        TestValueObject valueObject1 = new()
+        {
+            Value = 1
+        };
+
+        TestValueObject valueObject2 = null!; // Cast null false positive.
+
+        // Act
+        var result = valueObject1.Equals(valueObject2);
+
+        // Act & Assert
+        Assert.False(result);
     }
 
     private class TestValueObject : ValueObject
