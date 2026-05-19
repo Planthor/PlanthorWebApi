4210601582ef482c94635347ea160a7c4575b9af
Howard D. Le <45985646+akakshuki@users.noreply.github.com>
2026-05-19 11:25:29 +0700

    Change if condition syntax for directory check

--- .devcontainer/scripts/postCreateCommand.sh
+++ .devcontainer/scripts/postCreateCommand.sh
@@ -8,7 +8,7 @@ mkdir -p /home/vscode/.local/share/code/user
 chmod 755 /home/vscode/.local/share/code/user
 
 # Create symlink for VS Code to find settings in standard Linux location
-if [ ! -d /home/vscode/.config/Code ]; then
+if [[ ! -d /home/vscode/.config/Code ]]; then
   mkdir -p /home/vscode/.config
   ln -s /home/vscode/.local/share/code/user /home/vscode/.config/Code
 fi
